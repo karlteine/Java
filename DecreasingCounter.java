f3137d86df2b3b6f330b295ef5a8e90ec9940b13
karl <karlkivisild@gmail.com>
2024-09-10 14:34:58 +0300

    ex75 - using constructor to decrease the counter

---
+++ DecreasingCounter.java
@@ -0,0 +1,30 @@
+public class DecreasingCounter {
+    private int value;   // object variable that remembers the value of the counter
+
+    public DecreasingCounter(int valueAtStart) {
+        this.value = valueAtStart;
+    }
+
+    public void printValue() {
+        System.out.println("value: " + this.value);
+    }
+
+    public void decrease() {
+        // write here the code that decrements the value of counter by one
+        this.value--;
+    }
+    // and here the rest of the methods
+}
+class Main {
+    public static void main(String[] args) {
+        DecreasingCounter counter = new DecreasingCounter(10);
+
+        counter.printValue();
+
+        counter.decrease();
+        counter.printValue();
+
+        counter.decrease();
+        counter.printValue();
+    }
+}
