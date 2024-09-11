498978c2a26978e92d4d8e57764025aa31fd2eaf
karl <karlkivisild@gmail.com>
2024-09-11 15:50:30 +0300

    ex7 - created a dictionary program

---
+++ Dictionary.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+public class Dictionary {
+    private HashMap<String,String> translations;
+
+    public Dictionary() {
+        this.translations = new HashMap<>();
+    }
+
+    public String translate(String word) {
+        if (word == null || word.isEmpty()) {
+            return null;
+        } else {
+        return this.translations.get(word);
+        }
+    }
+    public void add(String word, String translation) {
+        this.translations.put(word, translation);
+    }
+    public static void main(String[] args) {
+        Dictionary dictionary = new Dictionary();
+        dictionary.add("apina", "monkey");
+        dictionary.add("banaani", "banana");
+        dictionary.add("cembalo", "harpsichord");
+
+        System.out.println(dictionary.translate("apina"));
+        System.out.println(dictionary.translate("porkkana"));
+    }
+}
