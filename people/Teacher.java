50146a781f239d8aced653eb5b86877b66556346
karl <karlkivisild@gmail.com>
2024-09-18 11:22:16 +0300

    ex29 - Implement Person, Teacher, and Student classes; add printDepartment method

---
+++ people/Teacher.java
@@ -0,0 +1,19 @@
+package people;
+
+public class Teacher extends Person {
+    private int salary;
+
+    public Teacher(String name, String address, int salary) {
+        super(name, address);
+        this.salary = salary;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\n salary " + salary + " euros/month";
+    }
+}
